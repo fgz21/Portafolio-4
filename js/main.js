50abe755d66b5e9dcd9566c88c5340993e854157
unknown <flowhister30@gmail.com>
2022-10-04 23:49:04 -0400

    renew incluid pdf

--- js/main.js
+++ js/main.js
@@ -3,14 +3,5 @@ let popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
   return new bootstrap.Popover(popoverTriggerEl)
 })
 
-function clip(){
-	copy1.innerText = copy1.innerText;
-	Copiado = copy1.createTextRange ();
-	Copied.execCommand ( "copia");
-	
-}
-
-copy();
-
 
 
